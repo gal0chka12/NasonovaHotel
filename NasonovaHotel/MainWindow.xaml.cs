0b3566dda74d9d69276b63d02bd50cdb3b0fb01a
Nasonova.G.V <Nasonova.G.V@PC205WIN-05>
2025-10-06 14:24:52 +0300

    Add project files.

---
+++ NasonovaHotel/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NasonovaHotel
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private async void BtnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            string username = txtUsername.Text.Trim();
+            string password = txtPassword.Password;
+            
+            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+            {
+                MessageBox.Show("Пожалуйста, введите логин и пароль");
+                return;
+            }
+
+            using (var context = new NasonovaHotelsEntities())
+            {
+                var user = await context.Users
+                    .Where(u => u.username == username)
+                    .FirstOrDefaultAsync();
+            }
+
+        }
+    }
+}
